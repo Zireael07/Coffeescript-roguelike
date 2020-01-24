612b3a49bb30654c5f0daec963b1116031f9590e
Zireael07 <zi@mail.com>
2020-01-24 21:08:01 +0100

    fix: typo

--- enums.js
+++ enums.js
@@ -4,23 +4,25 @@ import {struc_Tile } from './tile_lookup.js'
 const TileTypes = Object.freeze({
     WALL:   1,
     FLOOR:  2,
+    DEBUG:  3,
     data: {
       1 : new struc_Tile("wall", "#", true),
-      2 : new struc_Tile("floor", ".", false)
+      2 : new struc_Tile("floor", ".", false),
+      3 : new struc_Tile("debug", ">", false),
     }
   });
 
 
   const Directions = Object.freeze({
-    NORTH = [0, -1],
-    SOUTH = [0, 1],
-    EAST = [1, 0],
-    WEST = [-1, 0],
-    NORTHEAST = [1, -1],
-    NORTHWEST = [-1, -1],
-    SOUTHEAST = [1, 1],
-    SOUTHWEST = [-1, 1],
-    CENTER = [0,0],
+    NORTH : [0, -1],
+    SOUTH : [0, 1],
+    EAST : [1, 0],
+    WEST : [-1, 0],
+    NORTHEAST : [1, -1],
+    NORTHWEST : [-1, -1],
+    SOUTHEAST : [1, 1],
+    SOUTHWEST : [-1, 1],
+    CENTER : [0,0],
   })
 
  export { TileTypes, Directions } 
